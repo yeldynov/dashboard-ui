40b502fc3c2fcc59515c1e3f67fea18c42c23e58
Nikita Yeldynov <yeldynov@gmail.com>
2024-06-25 16:02:36 +0300

    + dark theme provider

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,6 +10,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import ThemeToggler from '@/components/ThemeToggler'
 
 const Navbar = () => {
   return (
@@ -25,25 +26,27 @@ const Navbar = () => {
           height={40}
         />
       </Link>
-
-      <DropdownMenu>
-        <DropdownMenuTrigger className='focus:outline-none'>
-          <Avatar>
-            <AvatarImage src='https://avatars.githubusercontent.com/u/52642286' />
-            <AvatarFallback className='text-black'>NY</AvatarFallback>
-          </Avatar>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>My Account</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href='/profile'>Profile</Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href='/auth'>Logout</Link>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className='flex items-center'>
+        <ThemeToggler />
+        <DropdownMenu>
+          <DropdownMenuTrigger className='focus:outline-none'>
+            <Avatar>
+              <AvatarImage src='https://avatars.githubusercontent.com/u/52642286' />
+              <AvatarFallback className='text-black'>NY</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href='/profile'>Profile</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href='/auth'>Logout</Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </div>
   )
 }
